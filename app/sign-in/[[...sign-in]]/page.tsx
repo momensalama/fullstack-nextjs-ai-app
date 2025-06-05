0bc870d1be242f2653d262bb8c33dec9c7f274a9
momen salama <momensalama712@gmail.com>
2025-06-05 13:52:56 +0300

    Enhance SignIn and SignUp components with custom styles and initial values

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <section className="flex h-screen w-screen items-center justify-center">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">Password: test123</h1>
+        <SignIn
+          appearance={{
+            elements: {
+              formButtonPrimary: "bg-slate-500 hover:bg-slate-400",
+              footerActionLink: "text-slate-500 hover:text-slate-400",
+            },
+          }}
+          initialValues={{
+            emailAddress: "momen+clerk_test@example.com",
+          }}
+        />
+      </div>
+    </section>
+  );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <section className="flex h-screen w-screen items-center justify-center">
+      <div className="flex flex-col gap-4">
+        <SignUp />
+      </div>
+    </section>
+  );
 }
